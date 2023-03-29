e1ee8ef9a7870b47d84d1abcb313ee253ade1b50
Siamesechen <arthuchenevans@gmail.com>
2023-03-29 09:21:02 +0100

    Created an MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace First_.NET_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
